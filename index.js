27b429ee8f34972e4de51e098341aa9e6c09734b
Macbook Pro M1 <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-06 18:46:45 +0300

    backend

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import multer from "multer";
+import fs from "fs";
+import mongoose, { get } from "mongoose";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+
+import { UserController, PostController } from "./Controllers/index.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
+import cors from "cors";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.ilcitek.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get("/posts", PostController.getAll);
+app.get("/tags", PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.get("/posts/tags", PostController.getLastTags);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK ");
+});
---
+++ utils/index.js
@@ -0,0 +1,2 @@
+export { default as checkAuth } from "../utils/checkAuth.js";
+export { default as handleValidationErrors } from "../utils/handleValidationErrors.js";
